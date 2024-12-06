67f724839546512ef5d8ff7474d267ad1802bbb7
artur-jansen <artur.jansenas@gmail.com>
2024-12-05 23:22:15 -0300

    Atualizando código

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const INSTAGRAM_ACCESS_TOKEN = 'IGQWRQM3JNYUZApQzE4eXc1Umgtd2d1Q2ZACTm9kNjV6ME1Sc0tGdE1MQlRIel8xeko5UkZAKZADlVM3NGZAEF1WWxJOTFOSzV5dHpaeTY5WVp0dnNPOHRpRndZAN3FNWlgtV1pJQ085am9FR21Xc1N0Qk9OSnhsdmpCcTAZD';
+// document.addEventListener('DOMContentLoaded', function() {
+//     const INSTAGRAM_ACCESS_TOKEN = 'IGQWRPUllFdEZABVE1ZAUHkwY1c3WFM1VWlOazZAQTS1mTEVMUDZAyMWFCUldMeGpaUlBkOURoMHlpVWVPRldnUVdBU2JNUWVoUEVZAN1VLaW1mdVJoejdvWkppX1Y1T0thVDlQbUljNWN3QmF5dE1TT2NMWFpjN0tUMWMZD';
 
-    async function fetchInstagramPosts() {
-        try {
-            const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${INSTAGRAM_ACCESS_TOKEN}`);
-            if (!response.ok) {
-                throw new Error(`Erro na solicitação: ${response.status} ${response.statusText}`);
-            }
-            const data = await response.json();
-            return data.data;
-        } catch (error) {
-            console.error('Erro ao buscar publicações do Instagram:', error);
-            return [];
-        }
-    }
+//     async function fetchInstagramPosts() {
+//         try {
+//             if (!INSTAGRAM_ACCESS_TOKEN) {
+//                 throw new Error('Token de acesso não fornecido.');
+//             }
 
-    function renderInstagramPosts(posts) {
-        if (!posts || posts.length === 0) {
-            console.error('Nenhuma publicação encontrada');
-            return;
-        }
+//             const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${INSTAGRAM_ACCESS_TOKEN}`);
+//             if (!response.ok) {
+//                 throw new Error(`Erro na solicitação: ${response.status} ${response.statusText}`);
+//             }
+//             const data = await response.json();
+//             return data.data;
+//         } catch (error) {
+//             console.error('Erro ao buscar publicações do Instagram:', error);
+//             return [];
+//         }
+//     }
 
-        const recentVideoPost = posts
-            .filter(post => post.media_type === 'VIDEO')
-            .sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime())
-            .slice(0, 1);
+//     function renderInstagramPosts(posts) {
+//         if (!posts || posts.length === 0) {
+//             console.error('Nenhuma publicação encontrada');
+//             return;
+//         }
 
-        const recentVideoPostContainer = document.getElementById('recent-video-post');
-        recentVideoPost.forEach(post => {
-            const videoElement = document.createElement('div');
-            videoElement.className = 'instagram-reel';
-            videoElement.innerHTML = `
-                <a href="${post.permalink}" target="_blank" rel="noopener noreferrer">
-                    <video class="instagram-post-reel" controls src="${post.media_url}"></video>
-                </a>
-            `;
-            recentVideoPostContainer.appendChild(videoElement);
-        });
+//         const recentVideoPost = posts
+//             .filter(post => post.media_type === 'VIDEO')
+//             .sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime())
+//             .slice(0, 1);
 
-        const instagramPostsContainer = document.getElementById('instagram-posts');
-        posts.slice(0, 4).forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.className = 'instagram-post';
-            postElement.innerHTML = `
-                <a href="${post.permalink}" target="_blank" rel="noopener noreferrer">
-                    <img class="instagram-post-feed" src="${post.media_url}" alt="${post.caption}">
-                </a>
-            `;
-            instagramPostsContainer.appendChild(postElement);
-        });
-    }
+//         const recentVideoPostContainer = document.getElementById('recent-video-post');
+//         recentVideoPost.forEach(post => {
+//             const videoElement = document.createElement('div');
+//             videoElement.className = 'instagram-reel';
+//             videoElement.innerHTML = `
+//                 <a href="${post.permalink}" target="_blank" rel="noopener noreferrer">
+//                     <video class="instagram-post-reel" controls src="${post.media_url}"></video>
+//                 </a>
+//             `;
+//             recentVideoPostContainer.appendChild(videoElement);
+//         });
 
-    fetchInstagramPosts().then(posts => {
-        renderInstagramPosts(posts);
-    });
-});
+//         const instagramPostsContainer = document.getElementById('instagram-posts');
+//         posts.slice(0, 4).forEach(post => {
+//             const postElement = document.createElement('div');
+//             postElement.className = 'instagram-post';
+//             postElement.innerHTML = `
+//                 <a href="${post.permalink}" target="_blank" rel="noopener noreferrer">
+//                     <img class="instagram-post-feed" src="${post.media_url}" alt="${post.caption}">
+//                 </a>
+//             `;
+//             instagramPostsContainer.appendChild(postElement);
+//         });
+//     }
+
+//     fetchInstagramPosts().then(posts => {
+//         renderInstagramPosts(posts);
+//     });
+// });
 
 document.addEventListener("DOMContentLoaded", function() {
     async function fetchData() {
@@ -91,20 +95,23 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener('DOMContentLoaded', function() {
     const toggler = document.getElementById('navbar-toggler');
     const navbarCollapse = document.getElementById('navbarNav');
-    
-    toggler.addEventListener('click', function() {
+
+    function updateTogglerIcon() {
         if (navbarCollapse.classList.contains('show')) {
-            toggler.classList.remove('toggler-icon-aberto');
-            toggler.classList.add('toggler-icon-fechado');
-        } else {
             toggler.classList.remove('toggler-icon-fechado');
             toggler.classList.add('toggler-icon-aberto');
+        } else {
+            toggler.classList.remove('toggler-icon-aberto');
+            toggler.classList.add('toggler-icon-fechado');
         }
+    }
+
+    toggler.addEventListener('click', function() {
+        setTimeout(updateTogglerIcon, 1); 
     });
 
-    if (navbarCollapse.classList.contains('show')) {
-        toggler.classList.add('toggler-icon-aberto');
-    } else {
-        toggler.classList.add('toggler-icon-fechado');
-    }
+    const observer = new MutationObserver(updateTogglerIcon);
+    observer.observe(navbarCollapse, { attributes: true, attributeFilter: ['class'] });
+
+    updateTogglerIcon();
 });
